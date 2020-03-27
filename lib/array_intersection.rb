8c7a5cdde51f98c13079931472a93b970b0a321f
Vera <lizeth.veraro@outlook.com>
2020-03-26 22:08:03 -0700

    Array_intersection hash table

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,19 @@
+
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # raise NotImplementedError, "Intersection not implemented"
+  intersec_result = []
+
+  hash = {}
+
+  list1.each do |number|
+    hash[number] = true
+  end
+
+  list2.each do |number|
+    if !hash[number].nil?
+      intersec_result << number
+    end
+  end
+
+  return intersec_result
 end
