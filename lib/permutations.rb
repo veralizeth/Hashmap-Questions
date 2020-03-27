2441b41682d1ec35d09408b906279d4e60ff6a30
Vera <lizeth.veraro@outlook.com>
2020-03-26 22:40:49 -0700

    Permutations solved

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,20 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  # raise NotImplementedError, "permutations? not implemented"
+  return false if string1.length != string2.length
+  # Return a boolean value.
+
+  hash = {}
+  
+  string1.each_char do |char|
+    hash[char] = true
+  end
+
+  string2.each_char do |letter|
+    if hash[letter] != true
+      return false
+    end
+  end
+
+  return true
 end
