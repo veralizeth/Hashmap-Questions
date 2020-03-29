bad3eacf70f648cdfe1bee5a657c032496adb27e
Vera <lizeth.veraro@outlook.com>
2020-03-29 13:42:45 -0700

    Refectoring code

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,20 +1,27 @@
 
+  
+def count_chars_in_string (string)
+  hash = {}
+
+  string.each_char do |char|
+    if hash.key?(char) 
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+
+  return hash
+end
+
 def permutations?(string1, string2)
   # raise NotImplementedError, "permutations? not implemented"
   return false if string1.length != string2.length
   # Return a boolean value.
 
-  hash = {}
-  
-  string1.each_char do |char|
-    hash[char] = true
-  end
+  hash_string1_counting = count_chars_in_string(string1)
+  hash_string2_counting = count_chars_in_string(string2)
 
-  string2.each_char do |letter|
-    if hash[letter] != true
-      return false
-    end
-  end
+  return hash_string1_counting == hash_string2_counting
 
-  return true
 end
